6146c0372e254f17068d284b49445d29ee522200
Vladimir_Pestov <Vladimir.Pestov@urfu.me>
2022-05-17 13:43:12 +0500

    Added moving platform

---
+++ Uncanny Platformer/Assets/Source/MovingTrap.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MovingTrap : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    private int currentTargetIndex;
+
+    [SerializeField] private float speed = 2f;
+
+    private Transform CurrentTarget => waypoints[currentTargetIndex];
+    
+    void Update()
+    {
+        if (Vector2.Distance(transform.position,
+            CurrentTarget.position) < 0.1f)
+        {
+            currentTargetIndex = (currentTargetIndex + 1) % waypoints.Length;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position,
+            CurrentTarget.position,
+            Time.deltaTime * speed);
+    }
+}
